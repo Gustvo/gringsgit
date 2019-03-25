51f0daebde72e40fc0bc2e1efa9a1f72769f615c
Gustavo Alves <Gustvo@github.com>
2019-03-25 20:35:56 -0300

    Alguem escreveu dobule

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 
-<<<<<<< HEAD
 int soma(int a,int b){
     int resultado = a + b;
     return resultado;
 }
 
-=======
 double divisao( double a, double b){
 	return (a/b);
 }
 
-double fatorial(dobule a){
+double fatorial(double a){
 	if(a>0){
 		a=a*fatorial(a-1);
 	}	
 }
->>>>>>> 66d2a6614ce6d2d0c2f729fb387e9066b90c38d3
 int main() {
     printf("Ola Mundo!");
     return 0;
