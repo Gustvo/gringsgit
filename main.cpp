fdac19547a2100049bd83cacc62054aca81d2b34
eduardo <eduardo@eduardo.com>
2019-03-25 20:20:43 -0300

    funcao fatorial

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ int soma(int a,int b){
 double divisao( double a, double b){
 	return (a/b);
 }
+
+double fatorial(dobule a){
+	if(a>0){
+		a=a*fatorial(a-1);
+	}	
+}
 >>>>>>> 66d2a6614ce6d2d0c2f729fb387e9066b90c38d3
 int main() {
     printf("Ola Mundo!");
